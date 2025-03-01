6d78bcd6d9468125b0acc94b69ffa1c8f9c1c690
crutiial <136341596+goaxxy@users.noreply.github.com>
2025-03-01 17:59:21 +0300

    Create Lib.lua

---
+++ Lib.lua
@@ -0,0 +1,131 @@
+local startArgs = {...} or {}
+
+if not game.Loaded then
+    repeat game.Loaded:Wait() until game:IsLoaded()
+end
+
+local Lib = {}
+
+do
+    Lib = {
+        IsOpen = true,
+        Categories = {},
+        Sections = {},
+        Notifications = {},
+        AccentObjects = {},
+        Keybinds = {
+            [Enum.KeyCode.Space] = "Space",
+            [Enum.KeyCode.Zero] = "0",
+            [Enum.KeyCode.One] = "1",
+            [Enum.KeyCode.Two] = "2",
+            [Enum.KeyCode.Three] = "3",
+            [Enum.KeyCode.Four] = "4",
+            [Enum.KeyCode.Five] = "5",
+            [Enum.KeyCode.Six] = "6",
+            [Enum.KeyCode.Seven] = "7",
+            [Enum.KeyCode.Eight] = "8",
+            [Enum.KeyCode.Nine] = "9",
+            [Enum.KeyCode.BackSlash] = "Backslash",
+            [Enum.KeyCode.QuotedDouble] = '"',
+            [Enum.KeyCode.Hash] = "#",
+            [Enum.KeyCode.Quote] = "'",
+            [Enum.KeyCode.Plus] = "+",
+            [Enum.KeyCode.Minus] = "-",
+            [Enum.KeyCode.Comma] = ",",
+            [Enum.KeyCode.Period] = ".",
+            [Enum.KeyCode.Colon] = ":",
+            [Enum.KeyCode.Semicolon] = ";",
+            [Enum.KeyCode.Equals] = "=",
+            [Enum.KeyCode.LeftBracket] = "[",
+            [Enum.KeyCode.RightBracket] = "]",
+            [Enum.KeyCode.LeftAlt] = "LAlt",
+            [Enum.KeyCode.RightAlt] = "RAlt",
+            [Enum.KeyCode.Backquote] = "`",
+            [Enum.KeyCode.Tilde] = "~",
+            [Enum.KeyCode.Delete] = "Del",
+            [Enum.KeyCode.CapsLock] = "Caps",
+            [Enum.KeyCode.LeftControl] = "LCtrl",
+            [Enum.KeyCode.RightControl] = "RCtrl",
+        },
+        Main = nil,
+        FontSize = 14,
+        UIOpenKey = Enum.KeyCode.RightShift,
+        CheatName = startArgs.CheatName or "Horizon",
+        FolderName = startArgs.FolderName or "Horizon",
+        Game = startArgs.Game or "FallenSurvival",
+        Logo = startArgs.Logo,
+        Accent = startArgs.Accent or Color3.fromRGB(0, 218, 151),
+    }
+
+    if not isfolder(Lib.FolderName) then
+        makefolder(Lib.FolderName)
+    end
+
+    if not isfolder(Lib.FolderName .. "/Configs") then
+        makefolder(Lib.FolderName .. "/Configs")
+    end
+
+    if not isfolder(Lib.FolderName .. "/Configs/" .. Lib.Game) then
+        makefolder(Lib.FolderName .. "/Configs/" .. Lib.Game)
+    end
+
+    Lib.__index = Lib
+    Lib.Categories.__index = Lib.Categories
+    Lib.Sections.__index = Lib.Sections
+    local TweenService = game:GetService("TweenService")
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local Mouse = LocalPlayer:GetMouse()
+    local UserInputService = game:GetService("UserInputService")
+
+    do
+        function Lib:SetConfig(Config)
+            
+        end
+        --
+        function Lib:GetConfig(Config)
+
+        end
+        --
+        function Lib:SetUIOpen(bool)
+            if typeof(bool) == 'boolean' then
+                Lib.IsOpen = bool
+                if Lib.IsOpen then
+                    Lib.Main.Visible = true
+                else
+                    Lib.Main.Visible = false
+                end
+            end
+        end
+        --
+    end
+
+    function Lib:Notificate(msg, dur)
+        local notif = {Objects = {}, Container = nil}
+
+        local NewIndex = Instance.new("TextLabel")
+        NewIndex.Size = UDim2.new(0,20,0,20)
+        NewIndex.Position = UDim2.new(0,20,0,20)
+        NewIndex.Text = message
+
+        function remove(item)
+            table.remove(Lib.Notifications, notif)
+        end
+
+        task.delay(dur + 0.1,function()
+            remove(notif)
+            NewIndex:Destroy()
+        end)
+
+        task.spawn(function()
+            TweenService:Create(NewIndex, TweenInfo.new(1, Enum.EasingStyle.Cubic, Enum.EasingDirection.InOut), {Size = UDim2.new(0,1,0,1)}):Play()
+
+            task.wait(dur)
+
+            TweenService:Create(NewIndex, TweenInfo.new(1, Enum.EasingStyle.Cubic, Enum.EasingDirection.InOut), {Size = UDim2.new(0,20,0,20)}):Play()
+        end)
+
+        table.insert(Lib.Notifications, notif)
+        return notif
+    end
+end
